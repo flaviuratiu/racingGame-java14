4afd70ca97b1443cfbfbbea40e665a4c77d87b19
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-11-27 20:03:26 +0200

    studied conditional statements (if-else)

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -28,8 +28,9 @@ public class App {
         System.out.println(carReference.engine.manufacturer);
         System.out.println(carReference.engine.capacity);
 
-        double accelerationDistance = carReference.accelerate(60, 1);
+        double accelerationDistance = carReference.accelerate(200, 1);
         System.out.println("Acceleration distance: " + accelerationDistance);
+        double accelerationDistance1 = carReference.accelerate(120, 1);
 
         Mechanic mechanic = new Mechanic();
         mechanic.repairVehicle(carReference);
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -19,8 +19,31 @@ public class Vehicle {
     }
 
     public double accelerate(double speed, double durationInHours) {
+        double mileageMultiplier = 1;
+
         System.out.println(name + " is accelerating with "
                 + speed + " for " + durationInHours + "h.");
+
+        if (speed > maxSpeed) {
+            System.out.println("Sorry. Maximum speed exceeded.");
+            return 0;
+        } else if (speed == maxSpeed) {
+            System.out.println("Careful! Max speed reached.");
+        } else {
+            System.out.println("Valid speed entered.");
+        }
+
+        if (fuelLevel <= 0) {
+            System.out.println("You don't have enough fuel.");
+            return 0;
+        }
+
+        if (speed > 120) {
+            System.out.println("Going fast... you'll use more fuel.");
+
+            // increasing mileage multiplier with percentage of acceleration speed
+            mileageMultiplier = speed / 100;
+        }
 
         // local variable (declared inside a method)
         double distance = speed * durationInHours;
@@ -31,11 +54,16 @@ public class Vehicle {
 
         System.out.println("Total traveled distance: " + traveledDistance);
 
-        double usedFuel = distance * mileage / 100;
+        double usedFuelWithStandardMileage = distance * mileage / 100;
+        System.out.println("Used fuel with standard mileage: " + usedFuelWithStandardMileage);
 
-        System.out.println("Used fuel: " + usedFuel);
+        double usedFuelWithCurrentMileage =
+                usedFuelWithStandardMileage * mileageMultiplier;
 
-        fuelLevel -= usedFuel;
+        System.out.println("Used fuel with current mileage: "
+                + usedFuelWithCurrentMileage);
+
+        fuelLevel -= usedFuelWithCurrentMileage;
 
         System.out.println("Remaining fuel level: " + fuelLevel);
 

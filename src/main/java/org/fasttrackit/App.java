99b23fb7c8a024f8b285e273914fef7a6295ec10
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-11-27 21:17:53 +0200

    studied encapsulation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -18,12 +18,12 @@ public class App {
         carEngine.capacity = 1.5;
 
         Car carReference = new Car(carEngine);
-        carReference.name = "Dacia";
-        carReference.color = "red";
-        carReference.mileage = 9.8;
-        carReference.fuelLevel = 60;
-        carReference.maxSpeed = 200;
-        carReference.running = false;
+        carReference.setName("Dacia");
+        carReference.setColor("red");
+        carReference.setMileage(9.8);
+        carReference.setFuelLevel(60);
+        carReference.setMaxSpeed(200);
+        carReference.setRunning(false);
         carReference.doorCount = 2;
 
         System.out.println("Engine details...");
@@ -38,47 +38,47 @@ public class App {
         mechanic.repairVehicle(carReference);
 
         System.out.println("Total traveled distance after repair: " +
-                carReference.traveledDistance);
+                carReference.getTraveledDistance());
 
         Engine car2Engine = new Engine();
         car2Engine.manufacturer = "Bavaria Motors";
         car2Engine.capacity = 2;
 
         Car car2 = new Car(car2Engine);
-        car2.name = "BMW";
-        car2.mileage = 14;
-        car2.color = null;
+        car2.setName("BMW");
+        car2.setMileage(14);
+        car2.setColor(null);
 
         // concatenation
         System.out.println(carReference);
-        System.out.println("First car name: " + carReference.name);
-        System.out.println(carReference.color);
-        System.out.println(carReference.mileage);
-        System.out.println(carReference.fuelLevel);
-        System.out.println(carReference.traveledDistance);
-        System.out.println(carReference.maxSpeed);
-        System.out.println(carReference.running);
+        System.out.println("First car name: " + carReference.getName());
+        System.out.println(carReference.getColor());
+        System.out.println(carReference.getMileage());
+        System.out.println(carReference.getFuelLevel());
+        System.out.println(carReference.getTraveledDistance());
+        System.out.println(carReference.getMaxSpeed());
+        System.out.println(carReference.isRunning());
         System.out.println(carReference.doorCount);
 
-        System.out.println("Second car name: " + car2.name);
-        System.out.println(car2.color);
-        System.out.println(car2.mileage);
-        System.out.println(car2.fuelLevel);
-        System.out.println(car2.traveledDistance);
-        System.out.println(car2.maxSpeed);
-        System.out.println(car2.running);
+        System.out.println("Second car name: " + car2.getName());
+        System.out.println(car2.getColor());
+        System.out.println(car2.getMileage());
+        System.out.println(car2.getFuelLevel());
+        System.out.println(car2.getTraveledDistance());
+        System.out.println(car2.getMaxSpeed());
+        System.out.println(car2.isRunning());
         System.out.println(car2.doorCount);
 
 
-        car2.name = "VW";
+        car2.setName("VW");
 
-        System.out.println(car2.name);
+        System.out.println(car2.getName());
 
         Car car3 = car2;
-        car3.name = "Audi";
+        car3.setName("Audi");
 
-        System.out.println("Car2 name: " + car2.name);
-        System.out.println("Car3 name: " + car3.name);
+        System.out.println("Car2 name: " + car2.getName());
+        System.out.println("Car3 name: " + car3.getName());
 
 // example for NullPointerException
 //        Car car4 = null;
